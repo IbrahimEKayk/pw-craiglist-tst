7048792d0adade758a7155dcbf23962da6b134e2
ibrahim E Kayikci <ibrahim.e.kayk@gmail.com>
2025-02-28 21:00:36 -0600

    Created test data and TDOs. Implemented screenshot methods

---
+++ testData/testDataObjects/filterTDO.ts
@@ -0,0 +1,9 @@
+export class FilterTDO {
+    readonly make_and_model: string
+    readonly year_minimum: string
+    readonly year_maximum: string
+    readonly miles_minimum: string
+    readonly miles_maximum: string
+    readonly price_minimum: string
+    readonly price_maximum: string
+}
